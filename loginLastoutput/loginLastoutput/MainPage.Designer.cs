e8d5b7afbe9f7c909799abadd016105a35042475
MagcalasGit <162887234+MagcalasGit@users.noreply.github.com>
2024-03-25 19:38:06 +0800

    Add files via upload

---
+++ loginLastoutput/loginLastoutput/MainPage.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace loginLastoutput
+{
+    partial class MainPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainPage));
+            label1 = new Label();
+            CheckBalBTn = new Guna.UI2.WinForms.Guna2Button();
+            withdrawBtn = new Guna.UI2.WinForms.Guna2Button();
+            cancelBtn = new Guna.UI2.WinForms.Guna2Button();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.DarkSlateGray;
+            label1.Font = new Font("Segoe UI Emoji", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Transparent;
+            label1.Location = new Point(361, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(159, 36);
+            label1.TabIndex = 0;
+            label1.Text = "Main Menu";
+            // 
+            // CheckBalBTn
+            // 
+            CheckBalBTn.BorderColor = Color.DarkCyan;
+            CheckBalBTn.BorderRadius = 15;
+            CheckBalBTn.BorderThickness = 2;
+            CheckBalBTn.CustomizableEdges = customizableEdges7;
+            CheckBalBTn.DisabledState.BorderColor = Color.DarkGray;
+            CheckBalBTn.DisabledState.CustomBorderColor = Color.DarkGray;
+            CheckBalBTn.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            CheckBalBTn.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            CheckBalBTn.FillColor = Color.DarkSlateGray;
+            CheckBalBTn.Font = new Font("Segoe UI Emoji", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            CheckBalBTn.ForeColor = Color.White;
+            CheckBalBTn.Location = new Point(350, 71);
+            CheckBalBTn.Name = "CheckBalBTn";
+            CheckBalBTn.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            CheckBalBTn.Size = new Size(180, 45);
+            CheckBalBTn.TabIndex = 3;
+            CheckBalBTn.Text = "Check Balance";
+            // 
+            // withdrawBtn
+            // 
+            withdrawBtn.BorderColor = Color.DarkCyan;
+            withdrawBtn.BorderRadius = 15;
+            withdrawBtn.BorderThickness = 2;
+            withdrawBtn.CustomizableEdges = customizableEdges9;
+            withdrawBtn.DisabledState.BorderColor = Color.DarkGray;
+            withdrawBtn.DisabledState.CustomBorderColor = Color.DarkGray;
+            withdrawBtn.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            withdrawBtn.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            withdrawBtn.FillColor = Color.DarkSlateGray;
+            withdrawBtn.Font = new Font("Segoe UI Emoji", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            withdrawBtn.ForeColor = Color.White;
+            withdrawBtn.Location = new Point(350, 122);
+            withdrawBtn.Name = "withdrawBtn";
+            withdrawBtn.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            withdrawBtn.Size = new Size(180, 45);
+            withdrawBtn.TabIndex = 4;
+            withdrawBtn.Text = "Withdraw";
+            // 
+            // cancelBtn
+            // 
+            cancelBtn.BorderColor = Color.DarkCyan;
+            cancelBtn.BorderRadius = 15;
+            cancelBtn.BorderThickness = 2;
+            cancelBtn.CustomizableEdges = customizableEdges11;
+            cancelBtn.DisabledState.BorderColor = Color.DarkGray;
+            cancelBtn.DisabledState.CustomBorderColor = Color.DarkGray;
+            cancelBtn.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            cancelBtn.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            cancelBtn.FillColor = Color.DarkSlateGray;
+            cancelBtn.Font = new Font("Segoe UI Emoji", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cancelBtn.ForeColor = Color.White;
+            cancelBtn.Location = new Point(350, 173);
+            cancelBtn.Name = "cancelBtn";
+            cancelBtn.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            cancelBtn.Size = new Size(180, 45);
+            cancelBtn.TabIndex = 5;
+            cancelBtn.Text = "Cancel";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(-1, -1);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(283, 234);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 6;
+            pictureBox2.TabStop = false;
+            // 
+            // MainPage
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PaleTurquoise;
+            ClientSize = new Size(588, 233);
+            Controls.Add(pictureBox2);
+            Controls.Add(cancelBtn);
+            Controls.Add(withdrawBtn);
+            Controls.Add(CheckBalBTn);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "MainPage";
+            Text = "MainPage";
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Guna.UI2.WinForms.Guna2Button CheckBalBTn;
+        private Guna.UI2.WinForms.Guna2Button withdrawBtn;
+        private Guna.UI2.WinForms.Guna2Button cancelBtn;
+        private PictureBox pictureBox2;
+    }
+}
